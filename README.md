35da52863057ff99e6f3d738aa7c3bd2bc3d6bb1
Alexander Luettig <alexander.luettig@googlemail.com>
2023-07-29 16:09:51 +0200

    progress

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rxnet-learning
+
+Small project to learn about Rx.Net and Observables in general.
---
+++ rx.console/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Reactive.Linq;
+
+// https://RxJs-fruits.com in C#
+
+#region Level 5: Filtering
+// var fruits = new[] { "apple", "apple", "old-apple", "apple", "old-apple", "banana", "old-banana", "old-banana", "banana", "banana" }.ToObservable();
+
+// fruits
+//     .Where(fruit => !fruit.Contains("old-"))
+//     .Subscribe(Console.WriteLine);
+#endregion
+
+#region Level 6: Select
+
+// var fruits = new[] { "dirty-apple", "apple", "dirty-banana", "banana" }.ToObservable();
+
+// fruits
+//     .Select(fruit => fruit.Replace("dirty-", ""))
+//     .Subscribe(Console.WriteLine);
+
+#endregion
+
+#region Level 7: Select, Where and Take
+
+// var fruits = new[] { "old-banana", "apple", "dirty-banana", "apple" }.ToObservable();
+
+// fruits
+//     .Where(fruit => !fruit.Contains("old-"))
+//     .Select(fruit => fruit.Replace("dirty-", ""))
+//     .Take(2)
+//     //.Distinct() (?)
+//     .Subscribe(Console.WriteLine);
+
+#endregion
+
+#region Level 8: SelectMany
+
+
+
+#endregion
